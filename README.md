a7f4f83d0de6584174dfc4e3f2742539d4153360
Jakub Gąsiorek <kubagasiorek6@gmail.com>
2024-06-27 10:06:39 +0200

    Start of a project

---
+++ README.md
@@ -0,0 +1,2 @@
+# React + Vite + HTML + CSS
+Creating Quizzical
---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import NotFound from './NotFound.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} errorElement={<NotFound />}>
+
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
