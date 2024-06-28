bf8b03d5af682d1e463227ee52474fa476b2b481
Jakub Gąsiorek <kubagasiorek6@gmail.com>
2024-06-28 15:29:40 +0200

    Added fetching data from API

---
+++ src/services/getQuestions.jsx
@@ -0,0 +1,24 @@
+export default function getQuestions(gameOptions) {
+    const { category, difficulty, type } = gameOptions;
+
+    let categoryQuery = "";
+	let difficultyQuery = "";
+	let typeQuery = "";
+
+    if(category !== ""){
+        categoryQuery = `&category=${category}`;
+    }
+
+    if(difficulty !== ""){
+        difficultyQuery = `&difficulty=${difficulty}`;
+    }
+
+    if(type !== ""){
+        typeQuery = `&type=${type}`;
+    }
+    
+    let apiUrl = `https://opentdb.com/api.php?amount=5${categoryQuery}${difficultyQuery}${typeQuery}`;
+    return fetch(apiUrl)
+    .then(res => res.json())
+    .then(data => data.results);
+}
