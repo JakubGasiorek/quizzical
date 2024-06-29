d18165ec1e29f803aa40d923e7d65444825e94cc
Jakub Gąsiorek <kubagasiorek6@gmail.com>
2024-06-29 18:11:44 +0200

    Added answeres to questions

--- src/components/Questions/Question.jsx
+++ src/components/Questions/Question.jsx
@@ -1,10 +1,26 @@
 /* eslint-disable react/prop-types */
 import { decode } from 'html-entities';
+import './Question.css';
+
 
 export default function Question(props) {
+    const answerElements = props.allAnswers.map(answer => (
+        <button 
+            key={answer.id} 
+            className={`answer-button ${props.selectedAnswer === answer.text ? 'selected' : ''}`}
+            onClick={() => props.handleSelectAnswer(props.id, answer.text)}
+        >
+            { decode(answer.text) }
+        </button>
+    ));
+
     return (
-        <div className=''>
+        <div className='question-container'>
             <h3 className="question-text">{ decode(props.question) }</h3>
+            <div className="answer-buttons">
+                { answerElements }
+            </div>
+            <hr />
         </div>
     )
 }
