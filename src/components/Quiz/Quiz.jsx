c4a345d474f6add820889c53c9bf20176509cc70
Jakub Gąsiorek <kubagasiorek6@gmail.com>
2024-06-30 16:57:24 +0200

    Added functionality that checks if answers are correct and count point if they are

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,79 +1,125 @@
-import Question from '../Questions/Question';
-import { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
-import { useLocation } from 'react-router-dom';
-import './Quiz.css';
+import Question from "../Questions/Question";
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+import { useLocation, useNavigate } from "react-router-dom";
+import "./Quiz.css";
 
 export default function Quiz() {
-	const [questionsArr, setQuestionsArr] = useState([]);
-	const [error, setError] = useState(false);
-	const location = useLocation();
-	const { questions } = location.state || { category: "", difficulty: "", type: "", questions: [] };
+  const [questionsArr, setQuestionsArr] = useState([]);
+  const [error, setError] = useState(false);
+  const [correctAnswersCount, setCorrectAnswersCount] = useState(0);
+  const [showScore, setShowScore] = useState(false);
 
-	function shuffleAnswers(array) {
-		for (let i = array.length - 1; i > 0; i--) {
-			const j = Math.floor(Math.random() * (i + 1));
-			[array[i], array[j]] = [array[j], array[i]];
-		}
-		return array;
-	}
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { questions } = location.state || {
+    category: "",
+    difficulty: "",
+    type: "",
+    questions: [],
+  };
 
-	useEffect(() => {
-		if (questions.length === 0) {
-			setError(true);
-			setQuestionsArr([]);
-		} else {
-			const formattedQuestions = questions.map(question => ({
-				...question,
-				id: nanoid(),
-				selectedAnswer: "",
-				showAnswer: false,
-				allAnswers: shuffleAnswers([
-					...question.incorrect_answers.map(answer => ({ text: answer, id: nanoid() })),
-					{ text: question.correct_answer, id: nanoid() }
-				])
-			}));
-			setQuestionsArr(formattedQuestions);
-			setError(false);
-		}
-	}, [questions]);
+  function shuffleAnswers(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
 
-	if (error) {
-		return <div>Error: No questions available or failed to fetch questions.</div>;
-	}
+  useEffect(() => {
+    if (questions.length === 0) {
+      setError(true);
+      setQuestionsArr([]);
+    } else {
+      const formattedQuestions = questions.map((question) => ({
+        ...question,
+        id: nanoid(),
+        selectedAnswer: "",
+        showAnswer: false,
+        allAnswers: shuffleAnswers([
+          ...question.incorrect_answers.map((answer) => ({
+            text: answer,
+            id: nanoid(),
+          })),
+          { text: question.correct_answer, id: nanoid() },
+        ]),
+      }));
+      setQuestionsArr(formattedQuestions);
+      setError(false);
+    }
+  }, [questions]);
 
-	const handleSelectAnswer = (questionId, answer) => {
-		setQuestionsArr(prevQuestionsArr => (
-			prevQuestionsArr.map(question => (
-				question.id === questionId
-					? { ...question, selectedAnswer: answer }
-					: question
-			))
-		));
-	}
+  if (error) {
+    return (
+      <div>Error: No questions available or failed to fetch questions.</div>
+    );
+  }
 
-	const questionElements = questionsArr.map(question => (
-		<Question
-			key={question.id}
-			id={question.id}
-			question={question.question}
-			correctAnswer={question.correct_answer}
-			incorrectAnswers={question.incorrect_answers}
-			difficulty={question.difficulty}
-			category={question.category}
-			selectedAnswer={question.selectedAnswer}
-			showAnswer={question.showAnswer}
-			allAnswers={question.allAnswers}
-			handleSelectAnswer={handleSelectAnswer}
-		/>
-	));
+  const handleSelectAnswer = (questionId, answer) => {
+    if (!showScore) {
+      setQuestionsArr((prevQuestionsArr) =>
+        prevQuestionsArr.map((question) =>
+          question.id === questionId
+            ? { ...question, selectedAnswer: answer }
+            : question
+        )
+      );
+    }
+  };
+  const handleCheckAnswers = () => {
+    let correctCount = 0;
+    const updatedQuestions = questionsArr.map((question) => {
+      if (question.selectedAnswer === question.correct_answer) {
+        correctCount++;
+      }
+      return { ...question, showAnswer: true };
+    });
+    setQuestionsArr(updatedQuestions);
+    setCorrectAnswersCount(correctCount);
+    setShowScore(true);
+  };
 
-	return (
-		<main>
-			<div className='questions-container'>{questionElements}</div>
-			<div className='controls-container'>
-				<button className="btn-check">Check answers</button>
-			</div>
-		</main>
-	);
+  const handleGoToStart = () => {
+    navigate("/");
+  };
+
+  const questionElements = questionsArr.map((question) => (
+    <Question
+      key={question.id}
+      id={question.id}
+      question={question.question}
+      correctAnswer={question.correct_answer}
+      selectedAnswer={question.selectedAnswer}
+      showAnswer={question.showAnswer}
+      allAnswers={question.allAnswers}
+      handleSelectAnswer={handleSelectAnswer}
+      showScore={showScore}
+    />
+  ));
+
+  return (
+    <main>
+      <div className="questions-container">{questionElements}</div>
+      <div className="controls-container">
+        <button
+          disabled={showScore}
+          className="btn-check"
+          onClick={handleCheckAnswers}
+        >
+          Check answers
+        </button>
+      </div>
+      {showScore && (
+        <>
+          <div className="score-container">
+            You scored {correctAnswersCount} out of {questionsArr.length}
+          </div>
+          <button className="btn-start" onClick={handleGoToStart}>
+            Play again
+          </button>
+        </>
+      )}
+    </main>
+  );
 }
